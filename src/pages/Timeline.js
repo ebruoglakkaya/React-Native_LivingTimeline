ffb902c779f2f62744a7ea9a24a34dffe6c02bce
mertkaanerdem <mmertkaanerdem@gmail.com>
2021-01-23 23:42:10 +0300

    chat input logical problems still continue

--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -24,7 +24,7 @@ function Timeline() {
         if (!data) {
           return;
         }
-        setTodoArray(Object.values(data));
+        setTimelineArray(Object.values(data));
       });
   }, []);
 
