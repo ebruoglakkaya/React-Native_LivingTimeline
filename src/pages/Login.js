847077f9bf60c17a2a2392283796d81d9f066ee3
mertkaanerdem <mmertkaanerdem@gmail.com>
2021-01-23 12:15:44 +0300

    'login page's logic nearly done'

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   SafeAreaView,
   Text,
@@ -6,11 +6,23 @@ import {
   TextInput,
   Image,
   View,
+  Alert,
 } from 'react-native';
 import {login_styles} from '../styles/pages_styles';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import auth from '@react-native-firebase/auth';
 
 function Login({navigation}) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function logIn() {
+    auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(() => navigation.navigate('Timeline'))
+      .catch(({code, message}) => Alert.alert(code, message));
+  }
+
   return (
     <SafeAreaView style={{flex: 1, backgroundColor: 'white'}}>
       <View style={login_styles.container}>
@@ -25,18 +37,19 @@ function Login({navigation}) {
 
         <View style={login_styles.eventContainer}>
           <TextInput
+            autoCapitalize="none"
             style={login_styles.input}
             placeholder="E-posta adresinizi giriniz"
+            onChangeText={(value) => setEmail(value)}
           />
 
           <TextInput
             style={login_styles.input}
             placeholder="Şifrenizi giriniz"
+            onChangeText={(value) => setPassword(value)}
           />
 
-          <TouchableOpacity
-            style={login_styles.button}
-            onPress={() => navigation.navigate('Timeline')}>
+          <TouchableOpacity style={login_styles.button} onPress={logIn}>
             <Text style={login_styles.btnTxt}>Giriş Yap</Text>
           </TouchableOpacity>
 
