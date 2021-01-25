ec24edb4020a87fe428046190ca6b9df4cd270a5
mertkaanerdem <mmertkaanerdem@gmail.com>
2021-01-25 18:38:43 +0300

    tab navigation icons

--- src/Router.js
+++ src/Router.js
@@ -3,6 +3,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {Login, Signup, Timeline, Favorites} from './pages';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import auth from '@react-native-firebase/auth';
 
 const Stack = createStackNavigator();
@@ -12,7 +13,15 @@ const hasSession = auth().currentUser;
 
 function TimelineTab() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color}) => generateIcon(focused, color, route),
+        tabBarLabel: () => null,
+      })}
+      tabBarOptions={{
+        activeTintColor: '#00838f',
+        inactiveTintColor: '#ffe082',
+      }}>
       <Tab.Screen name="Timeline" component={Timeline} />
       <Tab.Screen name="Favorites" component={Favorites} />
     </Tab.Navigator>
@@ -42,3 +51,22 @@ function Router() {
 }
 
 export default Router;
+
+function generateIcon(focused, color, route) {
+  let iconName;
+
+  switch (route.name) {
+    case 'Timeline':
+      iconName = focused ? 'timeline' : 'timeline-outline';
+      break;
+    case 'Favorites':
+      iconName = focused ? 'content-save' : 'content-save-outline';
+      break;
+
+    default:
+      break;
+  }
+
+  // You can return any component that you like here!
+  return <Icon name={iconName} color={color} size={30} />;
+}
